72dfa9b8ffc0c743c6addcc48b3d6c6d4086bd4b
limingyuebj <limingyuebj@fenbi.com>
2021-04-02 11:09:46 +0800

    chore: refactor folder structure

--- src/core/impl/window/window/window.spec.ts
+++ src/core/impl/window/window/window.spec.ts
@@ -1,4 +1,4 @@
-import { YBCWindowImpl } from '../window'
+import { YBCWindowImpl } from './window'
 test('adds 1 + 2 to equal 3', () => {
     const windowInstance = new YBCWindowImpl();
     expect(windowInstance.sum(1, 2)).toBe(3);
